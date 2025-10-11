c453bd04c86062193882f3c30dd629b42c278a55
Samad126 <samed192005@gmail.com>
2025-10-11 10:05:09 +0400

    initial swagger

--- src/api/src/main.ts
+++ src/api/src/main.ts
@@ -1,12 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const configService = app.get(ConfigService);
   const PORT = configService.get<number>('API_PORT');
   console.log(`Server started on port ${PORT}`);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cybercell Backend')
+    .setDescription('API Documentation')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'jwt',
+    )
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(PORT ?? 8000);
 }
 void bootstrap();
